d74b6d5100b059a364d8270a0c43d547438c8c60
Isuru Ariyarathna <isuruariyarathna2k00@gmail.com>
2023-06-01 08:09:59 +0530

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // styleUrls: ['./app.component.scss'],
+  styles: [
+    `
+      h1 {
+        color: red;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'hotel-inventory';
